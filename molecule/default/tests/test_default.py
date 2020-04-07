b8622472c2f04c0e28a36882f8110f1ee6583ed8
Lucas <lucas.maurice@outlook.com>
2020-04-07 04:36:53 -0400

    Implemented tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_motd_file_exist(host):
+def test_temporary_files_deleted(host):
     f = host.file('/tmp/acme.sh')
     assert not f.exists
 
-# def test_motd_file_work(host):
-#     host.run_test('/etc/profile.d/motd.sh')
+
+# def test_cert_created(host):
+#     f = host.file('/tmp/acme_test/test_role.justereseau.com.pem')
+#     assert f.exists
+
+
+# def test_key_created(host):
+#     f = host.file('/tmp/acme_test/test_role.justereseau.com.key')
+#     assert f.exists
